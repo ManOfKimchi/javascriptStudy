69d7501cb607df9ec79a6cef7ea8192ce4f8f4a6
ManOfKimchi <40424886+ManOfKimchi@users.noreply.github.com>
2021-03-20 19:11:08 +0900

    17.3

---
+++ 리액트를다루는기술/react-redux-tutorial/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Counter from './components/Counter';
+import Todos from './components/Todos';
+
+const App = () => {
+    return (
+        <div>
+            <Counter number={0}></Counter>
+            <hr></hr>
+            <Todos></Todos>
+        </div>
+    );
+};
+
+export default App;
