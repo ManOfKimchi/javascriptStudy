9991af825dc378d324147b3416691d54916d17d4
ManOfKimchi <40424886+ManOfKimchi@users.noreply.github.com>
2021-05-05 19:17:53 +0900

    20.3

--- 리액트를다루는기술/ssr-recipe/src/index.server.js
+++ 리액트를다루는기술/ssr-recipe/src/index.server.js
@@ -1,6 +1,69 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
+import express from 'express';
+import { StaticRouter } from 'react-router-dom';
+import App from './App';
+import path from 'path';
+import fs from 'fs';
 
-const html = ReactDOMServer.renderToString(<div>Hello SSR</div>);
+const manifest = JSON.parse(
+    fs.readFileSync(path.resolve('./build/asset-manifest.json'), 'utf-8')
+);
 
-console.log(html);
+const chunks = Object.keys(manifest.files)
+    .filter((key) => /chunk\.js$/.exec(key))
+    .map((key) => `<script src="${manifest.files[key]}"></script>`)
+    .join('');
+
+function createPage(root, tags) {
+    return `<!DOCTYPE html>
+          <html lang="en">
+          <head>
+            <meta charset="utf-8" />
+            <link rel="shortcut icon" href="/favicon.ico" />
+            <meta
+              name="viewport"
+              content="width=device-width,initial-scale=1,shrink-to-fit=no"
+            />
+            <meta name="theme-color" content="#000000" />
+            <title>React App</title>
+            <link href="${manifest.files['main.css']}" rel="stylesheet" />
+          </head>
+          <body>
+            <noscript>You need to enable JavaScript to run this app.</noscript>
+            <div id="root">
+              ${root}
+            </div>
+            <script src="${manifest.files['runtime-main.js']}"></script>
+            ${chunks}
+            <script src="${manifest.files['main.js']}"></script>
+          </body>
+          </html>
+            `;
+}
+
+const app = express();
+
+const serverRender = (req, res, next) => {
+    // 404 떠야 할 때 SSR 해줌
+    const context = {};
+    const jsx = (
+        <StaticRouter location={req.url} context={context}>
+            <App></App>
+        </StaticRouter>
+    );
+    const root = ReactDOMServer.renderToString(jsx);
+    res.send(createPage(root));
+};
+
+const serve = express.static(path.resolve('./build'), {
+    index: false,
+});
+
+// 서순 중요
+app.use(serve);
+app.use(serverRender);
+
+app.listen(5000, () => {
+    console.log('Running on http://localhost:5000');
+});
