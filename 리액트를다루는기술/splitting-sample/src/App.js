50e369f25e622a0da7dd8f020f078cdb7dbf9cec
ManOfKimchi <40424886+ManOfKimchi@users.noreply.github.com>
2021-05-05 18:01:02 +0900

    19 끝

--- 리액트를다루는기술/splitting-sample/src/App.js
+++ 리액트를다루는기술/splitting-sample/src/App.js
@@ -2,22 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 // import notify from './notify';
 import React, { Component, useState, Suspense } from 'react';
+// SSR 할때 사용하기를 권장하는 라이브러리
+import loadable from '@loadable/component';
 
-const SplitMe = React.lazy(() => import('./SplitMe'));
+const SplitMe = loadable(() => import('./SplitMe'), {
+    fallback: <div>Loading...</div>,
+});
 
 function App() {
     const [visible, setVisible] = useState(false);
     const onClick = () => {
         setVisible(!visible);
     };
+    const onMouseOver = () => {
+        SplitMe.preload();
+    };
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"></img>
-                <p onClick={onClick}>Hello!</p>
-                <Suspense fallback={<div>Loading...</div>}>
-                    {visible && <SplitMe></SplitMe>}
-                </Suspense>
+                <p onClick={onClick} onMouseOver={onMouseOver}>
+                    Hello!
+                </p>
+                {visible && <SplitMe></SplitMe>}
             </header>
         </div>
     );
