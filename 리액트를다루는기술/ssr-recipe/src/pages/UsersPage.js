4475cc122797c0ed6359669d47dd748720d3e619
ManOfKimchi <40424886+ManOfKimchi@users.noreply.github.com>
2021-05-05 20:09:46 +0900

    20.4.2

---
+++ 리액트를다루는기술/ssr-recipe/src/pages/UsersPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import UsersContainer from '../containers/UsersContainer';
+
+const UsersPage = () => {
+    return <UsersContainer></UsersContainer>;
+};
+
+export default UsersPage;
