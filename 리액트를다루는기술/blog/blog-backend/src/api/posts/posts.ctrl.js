347641bdfaaddb88f68953356f9f92607f1fbdda
ManOfKimchi <40424886+ManOfKimchi@users.noreply.github.com>
2021-06-19 18:08:54 +0900

    21 끝

---
+++ 리액트를다루는기술/blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -0,0 +1,97 @@
+const { post } = require('.');
+
+let postId = 1;
+
+const posts = [
+  {
+    id: 1,
+    title: 'title',
+    body: 'content',
+  },
+  {
+    id: 2,
+    title: 'hihihihihi',
+    body: 'bodybodybodybody',
+  },
+];
+
+// post
+exports.write = (ctx) => {
+  const { title, body } = ctx.request.body;
+  postId += 1;
+  const post = { id: postId, title, body };
+  posts.push(post);
+  ctx.body = post;
+};
+
+// get
+exports.list = (ctx) => {
+  ctx.body = posts;
+};
+exports.read = (ctx) => {
+  const { id } = ctx.params;
+  const post = posts.find((p) => p.id.toString() === id);
+  if (!post) {
+    ctx.status = 404;
+    ctx.body = {
+      message: 'post not found',
+    };
+    return;
+  }
+
+  ctx.body = post;
+};
+
+// delete
+exports.remove = (ctx) => {
+  const { id } = ctx.params;
+  const index = posts.findIndex((p) => p.id.toString() === id);
+  if (index === -1) {
+    ctx.status = 404;
+    ctx.body = {
+      message: 'post not found',
+    };
+    return;
+  }
+
+  post.splice(index, 1);
+  ctx.status = 204;
+};
+
+// put
+exports.replace = (ctx) => {
+  const { id } = ctx.params;
+  const index = posts.findIndex((p) => p.id.toString() === id);
+  if (index === -1) {
+    ctx.status = 404;
+    ctx.body = {
+      message: 'post not found',
+    };
+    return;
+  }
+
+  posts[index] = {
+    id,
+    ...ctx.request.body,
+  };
+  ctx.body = posts[index];
+};
+
+// patch
+exports.update = (ctx) => {
+  const { id } = ctx.params;
+  const index = posts.findIndex((p) => p.id.toString() === id);
+  if (index === -1) {
+    ctx.status = 404;
+    ctx.body = {
+      message: 'post not found',
+    };
+    return;
+  }
+
+  posts[index] = {
+    ...posts[index],
+    ...ctx.request.body,
+  };
+  ctx.body = posts[index];
+};
--- 리액트를다루는기술/blog/blog-backend/src/index.js
+++ 리액트를다루는기술/blog/blog-backend/src/index.js
@@ -1,25 +1,29 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+const api = require('./api');
+const bodyParser = require('koa-bodyparser');
+
 const app = new Koa();
+const router = new Router();
 
-app.use(async (ctx, next) => {
-  console.log(ctx.url);
-  console.log(1);
-  if (ctx.query.authorized !== '1') {
-    ctx.status = 401;
-    return;
-  }
-  await next();
-  console.log('END');
-});
+router.use('/api', api.routes());
 
-app.use((ctx, next) => {
-  console.log(2);
-  next();
-});
+// router.get('/', (ctx) => {
+//   ctx.body = 'home';
+// });
+// // param
+// router.get('/about/:name?', (ctx) => {
+//   const { name } = ctx.params;
+//   ctx.body = name ? `about ${name}` : 'introduce';
+// });
+// // query
+// router.get('/posts', (ctx) => {
+//   const { id } = ctx.query;
+//   ctx.body = id ? `post #${id}` : 'post id is null';
+// });
 
-app.use((ctx) => {
-  ctx.body = 'hello world';
-});
+app.use(bodyParser());
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
   console.log('listening to port 4000');
