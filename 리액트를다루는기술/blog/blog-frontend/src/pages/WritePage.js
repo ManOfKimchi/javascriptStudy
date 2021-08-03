5067e582fafe2d33787d4f5cdd42726e62ecc45a
ManOfKimchi <40424886+ManOfKimchi@users.noreply.github.com>
2021-08-03 20:01:03 +0900

    25 end

--- 리액트를다루는기술/blog/blog-frontend/src/pages/WritePage.js
+++ 리액트를다루는기술/blog/blog-frontend/src/pages/WritePage.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Responsive from '../components/common/Responsive';
-import WriteActionButtons from '../components/write/WriteActionButtons';
 import EditorContainer from '../containers/write/EditorContainer';
 import TagBoxContainer from '../containers/write/TagBoxContainer';
+import WriteActionButtonsContainer from '../containers/write/WriteActionButtonsContainer';
 
 const WritePage = () => {
     return (
         <Responsive>
             <EditorContainer></EditorContainer>
             <TagBoxContainer></TagBoxContainer>
-            <WriteActionButtons></WriteActionButtons>
+            <WriteActionButtonsContainer></WriteActionButtonsContainer>
         </Responsive>
     );
 };
