fe0a4ed4dd7a10001f06ab356c304e982904fa51
ManOfKimchi <40424886+ManOfKimchi@users.noreply.github.com>
2021-08-16 20:37:33 +0900

    27 end

--- 리액트를다루는기술/blog/blog-backend/src/main.js
+++ 리액트를다루는기술/blog/blog-backend/src/main.js
@@ -6,6 +6,9 @@ import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 // import createFakeData from './createFakeData';
 import jwtMiddleware from './lib/jwtMiddleware';
+import serve from 'koa-static';
+import path from 'path';
+import send from 'koa-send';
 
 const { PORT, MONGO_URI } = process.env;
 const app = new Koa();
@@ -39,6 +42,14 @@ router.use('/api', api.routes());
 app.use(bodyParser());
 app.use(jwtMiddleware);
 app.use(router.routes()).use(router.allowedMethods());
+
+const buildDirectory = path.resolve(__dirname, '../../blog-frontend/build');
+app.use(serve(buildDirectory));
+app.use(async (ctx) => {
+    if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+        await send(ctx, 'index.html', { root: buildDirectory });
+    }
+});
 
 const port = PORT || 4000;
 app.listen(port, () => {
